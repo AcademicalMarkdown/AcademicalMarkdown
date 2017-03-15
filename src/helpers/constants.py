ccceb435590af279a0f3ac1edc68b46fde53feb4
Zhang Cheng <13501393281@163.com>
2017-03-15 14:36:42 +0000

    minor fix

--- src/helpers/constants.py
+++ src/helpers/constants.py
@@ -1,12 +1,14 @@
 # constants are invariant outside of this file.
 
+# this is used for formatting due to python formatting will have an error
+# when you try to format "{{a}, {b}}" because of the outer brackets
+LEFT_BRACKET = '{'
+RIGHT_BRACKET = '}'
+
+
 LATEX_CODE_FORMAT_STR = '''
 ~~~~{left_bracket}.{syntax} caption="{caption}" label="{label}"{right_bracket}
 {content}
 ~~~~
 '''
 
-# this is used for formatting due to python formatting will have an error
-# when you try to format "{{a}, {b}}" because of the outer brackets
-LEFT_BRACKET = '{'
-RIGHT_BRACKET = '}'
