2127f55e5ff310800df482d84dbf7261f07a3b83
Zhang Cheng <13501393281@163.com>
2017-03-16 14:03:18 +0000

    add documentation

--- src/compilers/to_latex/include_compiler_latex.py
+++ src/compilers/to_latex/include_compiler_latex.py
@@ -8,8 +8,8 @@ from src.models.include_model import IncludeModel
 class IncludeCompilerLatex(IncludeModel, BaseCompiler):
     def compile(self) -> str:
         """
-        find the file
-        :return:
+        find the file and copy the content of the file into current file
+        :return: the content of the file to include
         """
         try:
             with codecs.open(self.file, 'r', encoding='utf-8') as f:
