32bd3b5fa39db35f7e4fb5132e1f763835cf3121
Zhang Cheng <13501393281@163.com>
2017-03-17 18:55:06 +0000

    remove the load_yaml method

--- src/compilers/base_compiler.py
+++ src/compilers/base_compiler.py
@@ -1,6 +1,3 @@
-import yaml
-
-from src.helpers.general_function import positional_to_keyword_para
 from src.models.base_model import BaseModel
 
 
@@ -18,21 +15,6 @@ class BaseCompiler(BaseModel):
         this is inherited from BaseModel, see models/base_model.py for more
         """
         raise NotImplementedError
-
-    def load_yaml(self, input_yaml: str):
-        """
-        [Deprecated]
-        load the data in the yaml file into current model
-        :param input_yaml:
-        """
-        # load the block parameter
-        paras = yaml.safe_load(input_yaml)
-        # convert the parameters to keyword parameters
-        key_paras = positional_to_keyword_para(
-            model=self, paras=paras
-        )
-        # load the keyword parameter
-        self.load_dict(key_paras)
 
     def compile(self) -> str:
         raise NotImplementedError
