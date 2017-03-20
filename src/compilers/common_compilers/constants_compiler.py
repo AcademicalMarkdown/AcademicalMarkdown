f34a51189e860335d1fab01dc2e81a0004d06435
Zhang Cheng <13501393281@163.com>
2017-03-20 18:51:41 +0000

    change the lambda to def for PEP8

--- src/compilers/common_compilers/constants_compiler.py
+++ src/compilers/common_compilers/constants_compiler.py
@@ -17,9 +17,10 @@ def const_compile(document: str) -> str:
         # find the reference pattern and the string to replace it with
         reference_pattern = ORIG_CONST_REGEX_FORMAT.format(
             label=const_tuple[0])
+
         # use lambda to escape characters in const_tuple[1]
         # like \1 or \
-        replace_func = lambda x: const_tuple[1]
+        def replace_func(_): return const_tuple[1]
 
         # replace everything in that document
         document = re.sub(pattern=reference_pattern,
