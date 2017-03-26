8465d427824746b6d871b8d0f1fda983c79e9694
Zhang Cheng <13501393281@163.com>
2017-03-26 21:51:37 +0800

    remove the position_to_keyword function

--- src/helpers/general_function.py
+++ src/helpers/general_function.py
@@ -1,40 +1,9 @@
 import re
 
 import yaml
-from typing import List, Union
 
 from src.helpers.constants import UNESCAPED_REGEX_SUB_LIST, \
     YAML_PARSE_ERROR_FORMAT
-from src.models.base_model import BaseModel
-
-
-def positional_to_keyword_para(model: BaseModel,
-                               paras: Union[dict, List]) -> dict:
-    """
-    this function converts positional parameter (a list of parameter)
-    to keyword parameter (a dictionary of parameter)
-    the positional parameter info can use get_positional method on model to get
-
-    :param model: the type of the model, see the models folder
-    :param paras: the parameters, either a list (positional) or a dict(keyword)
-    :return: purely keyword parameter (dict)
-    """
-
-    # this is already a keyword para
-    if isinstance(paras, dict):
-        return paras
-
-    # this is not a keyword para
-    positional_para = model.get_content_data_name()
-
-    res_paras = {}
-    for index, para in enumerate(paras):
-        if isinstance(para, dict):
-            res_paras.update(para)
-        else:
-            res_paras.update({positional_para[index]: para})
-
-    return res_paras
 
 
 def unescape_block(block_str: str) -> str:
