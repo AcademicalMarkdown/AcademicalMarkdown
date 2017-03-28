e6a01ce9da3fb986f04b46814418d02ef638f8a3
Zhang Cheng <13501393281@163.com>
2017-03-28 12:23:32 +0800

    add the main file for freeze

---
+++ mdac.py
@@ -0,0 +1,10 @@
+import sys
+
+from fire import Fire
+
+from info import PROJECT_ROOT_PATH
+from src.__main__ import main
+
+if __name__ == '__main__':
+    sys.path.append(PROJECT_ROOT_PATH)
+    Fire(main)
